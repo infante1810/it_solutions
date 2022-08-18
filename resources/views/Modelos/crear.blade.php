66aca255788b361b6dcadd87238ad4d4a9cd6386
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-18 10:36:06 -0500

    update modelos views

--- resources/views/Modelos/crear.blade.php
+++ resources/views/Modelos/crear.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Agregar modelo</h4>
+        <h4 class="page-title">REGISTRO 2</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Registro 2</a></li>
+            <li class="mb-1 fs-16"><a href="{{ route('bd.index') }}">Registro2</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Agregar modelo</li>
+            <li class="text-muted mb-1 fs-16">Agregar Registro</li>
         </ul>
     </div>
 </div>
@@ -18,63 +18,103 @@
 
 
 
+<!-- CONTENIDO -->
+
+
+
 <div class="row">
-			<div class="col-xl-12 col-md-12 col-lg-12">
-				<div class="card">
-					<form>
-					<div class="card-body">
-						<h4 class="mb-5 font-weight-semibold">Información del modelo</h4>
-						<br>
-                        <div class="row">
-                            <div class="col-md-4">
-								<div class="form-group">
-									<label class="form-label">Nombre</label>
-									<input required class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Ingresa el nombre del modelo" name="nombreModelo" maxlength="40"  value="{{ old('nombreModelo') }}>
-									@error('nombreModelo')
-										<span class="invalid-feedback" role="alert">
-											<strong> {{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
-							</div>
-                            <div class="col-md-4">
-								<div class="form-group">
-									<label class="form-label" for="start">Fecha</label>
-									<input required class="form-control @error('start_date') is-invalid @enderror" type="date" id="start" value="2022-04-02" min="2016-01-01" max="2030-12-31"  value="{{ old('start_date') }}">
-									@error('start_date')
-										<span class="invalid-feedback" role="alert">
-											<strong> {{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
-							</div>
-                        </div>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="form-group">
-									<label class="form-label">Descripción</label>
-                                    <textarea required class="form-control @error('descripcionModelo') is-invalid @enderror" type="text" rows="6" placeholder="Ingresa la descripción del modelo" name="descripcionModelo"></textarea>
-									@error('descripcionModelo')
-										<span class="invalid-feedback" role="alert">
-											<strong> {{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
-							</div>
+	<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+			<form action="{{route('bd.store')}}" method="POST">
+			@csrf
+			<div class="card-body">
+
+				<h4 class="mb-5 font-weight-semibold">Crea un registro</h4>
+				<div class="row">
+					<div class="col-md-3">
+						<div class="form-group">
+							<label for="name" class="form-label">Nombre</label>
+							<input id="name" class="form-control @error('name') is-invalid @enderror" type="text" maxlength="22"
+                                   name="name" autofocus required value="{{ old('name') }}">
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong> {{ $message }}</strong>
+                                </span>
+                            @enderror
 						</div>
 					</div>
-					<div class="card-footer text-right">
-						<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
-							<i class="feather feather-corner-down-left sidemenu_icon"></i>
-							Regresar
-						</a>
-						<button type="submit" class="btn btn-primary">
-							<i class="feather  feather-save sidemenu_icon"></i>
-							Guardar</button>
+				<!--<div class="col-md-3">
+						<div class="form-group">
+							<label for="location" class="form-label">Localización</label>
+							<input id="location" class="form-control @error('location') is-invalid @enderror" type="text" maxlength="100"
+                                   name="location" required value="{{ old('location') }}">
+                            @error('location')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong> {{ $message }}</strong>
+                                </span>
+                            @enderror
+						</div>
 					</div>
-				</form>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label for="views_counter" class="form-label">Numero de vistas</label>
+							<input id="views_counter" class="form-control @error('views_counter') is-invalid @enderror" type="number" maxlength="30"
+                                   name="views_counter" required min="1" value="{{ old('views_counter') }}">
+                            @error('views_counter')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong> {{ $message }}</strong>
+                                </span>
+                            @enderror
+						</div>
+					</div>-->
 				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="description" class="form-label">Descripción</label>
+							<textarea id="description" class="form-control @error('description') is-invalid @enderror" type="text" rows="6"
+                                      placeholder="Ingresa el contenido de tu registro" name="description" required
+                                      value="{{ old('description') }}"></textarea>
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong> {{ $message }}</strong>
+                                </span>
+                            @enderror
+						</div>
+					</div>
+				</div>
+
+                <div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label for="total_cost" class="form-label">Costo total</label>
+							<input id="total_cost" class="form-control @error('total_cost') is-invalid @enderror" type="number" step="0.01"
+                                   placeholder="50.00" name="total_cost" required min="0" value="{{ old('total_cost') }}">
+                            @error('total_cost')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong> {{ $message }}</strong>
+                                </span>
+                            @enderror
+						</div>
+					</div>
+				</div>
+
+				<input type="hidden" name="user_id" value="1">
 			</div>
+
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+				<button type="submit" class="btn btn-primary" id="enviar">
+					<i class="feather  feather-save sidemenu_icon"></i>
+					Guardar</button>
+			</div>
+		</form>
 		</div>
+	</div>
+</div>
 		<!-- End Row-->
 @endsection
--- resources/views/Modelos/editar.blade.php
+++ resources/views/Modelos/editar.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Editar modelo</h4>
+        <h4 class="page-title">Editar registro</h4>
         <ul class="breadcrumb">
             <li class="mb-1 fs-16"><a  href="{{ route('registro2.index') }}">Registro 2</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Editar modelo</li>
+            <li class="text-muted mb-1 fs-16">Editar registro</li>
         </ul>
     </div>
 </div>
