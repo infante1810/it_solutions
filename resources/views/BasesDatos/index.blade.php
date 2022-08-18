08c4784ce0be9e77fba080556ebba3b828ecf5fb
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-18 10:29:02 -0500

    update databases views

--- resources/views/BasesDatos/index.blade.php
+++ resources/views/BasesDatos/index.blade.php
@@ -5,60 +5,74 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Bases de Datos</h4>
-        <h4 class="page-subtitle">Base de Datos / Soluciones TI</h4>
+        <h4 class="page-title">Registros</h4>
     </div>
     <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
-            <div class="btn-list"> 
-                <!-- LINK DE LA BASE DE DATOS EN phpmyadmin -->
-                <a href="http://google.com" target=_blanck class="btn btn-primary mr-3">
-                    Ir a Base de Datos</a>
+            <div class="btn-list">
+                <a href="{{route('bd.create')}}" class="btn btn-primary mr-3">
+                    <i class="fa-solid fa-plus"></i>
+                    Agregar Registro
+                </a>
             </div>
         </div>
     </div>
 </div>
-<!-- FIN CABECERA -->
-
-
+<!--End Page header-->
 
 
 <!-- CONTENIDO -->
+
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
         <div class="card">
+            <div class="card-header  border-0">
+                <h4 class="card-title">Lista de productos/servicios</h4>
+            </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table  table-vcenter text-wrap table-bordered border-bottom dt" id="project-list">
+                    <table class="table table-vcenter text-wrap table-bordered border-bottom dt" id="tabla">
                         <thead>
                             <tr>
-                                <th class="border-bottom-0">Recursos</th>
-                                <th class="border-bottom-0">Descripción</th>
-                                <th class="border-bottom-0">Acceso Directo</th> 
+                                <th class="border-bottom-0">ID</th>
+                                <th class="border-bottom-0">Servicio/Producto</th>
+                                <th class="border-bottom-0">Descripcion</th>
+                                <th class="border-bottom-0">Costos</th>
+                                <th class="border-bottom-0" Style="text-align: center;">Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($databases as $database)
                             <tr>
+                                <td>{{$database->id}}</td>
                                 <td>
                                     <div class="d-flex">
                                         <div class="mr-3 mt-0 mt-sm-1 d-block">
-                                            <!--<h6 class="mb-1 fs-14">Empleados</h6>-->
-                                            <h6 class="mb-1 fs-14">Empleados</h6>
+                                            <h6 class="mb-1 fs-14">{{$database->name}}</h6>
                                         </div>
                                     </div>
                                 </td>
-                                <td width="350px">
-                                    <!--<span>Se muestran los datos de los empleados de la empresa</span>-->
-                                    <span>$20,000.00</span>
+                                <td>
+                                    <span>{{$database->description}}</span>
                                 </td>
                                 <td>
-                                    <!--Aqui lleva el link directo a cada tabla en la BD-->
-                                    <a href="http://google.com" target=_blanck class="action-btns1" data-toggle="tooltip" data-placement="top" title="Ver"><i class="fa-solid fa-eye text-primary"></i></a>
-                                </a>
-                                    </span>
+                                    <span>{{$database->total_cost}}</span>
+                                </td>
+                                <td>
+                                    <div class="d-flex">
+                                        <a href="{{route('bd.show',$database->id)}}" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Ver"><i class="fa-solid fa-eye text-primary"></i></a>
+                                        <a href="{{route('bd.edit',$database->id)}}" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa-solid fa-pen text-success"></i></a>
+                                        <form action="{{route('bd.destroy', $database->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="action-btns1" data-toggle="tooltip" data-placement="top" title="Eliminar" type="submit"><i class="fa-regular fa-trash-can text-danger"></i></button>
+                                        </form>
+                                    </div>
                                 </td>
                             </tr>
+                            @endforeach
                         </tbody>
+
                     </table>
                 </div>
             </div>
@@ -67,3 +81,46 @@
 </div>
 <!-- FIN CONTENIDO -->
 @endsection
+
+
+@section('extra-script')
+<script type="text/javascript">
+    function mensaje(){
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-outline-dark'
+          },
+          buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+          title: '¿Eliminar registro?',
+          text: "Si eliminas a este registro se eliminará permanentemente del sistema.",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Si, eliminar registro!.',
+          cancelButtonText: 'No, mantener registro!.',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              'El registro se ha eliminado correctamente.',
+              'success'
+            )
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire(
+              'Proceso cancelado.',
+              '',
+              'error'
+            )
+          }
+        })
+    }
+</script>
+@endsection
+
---
+++ resources/views/BasesDatos/ver.blade.php
@@ -0,0 +1,81 @@
+@extends('templates.main')
+
+@section('content')
+
+<!-- CABECERA -->
+<div class="page-header d-xl-flex d-block">
+    <div class="page-leftheader">
+        <h4 class="page-title">Ver registro</h4>
+        <ul class="breadcrumb">
+            <li class="mb-1 fs-16"><a href="{{ route('bd.index') }}">Registros 3</a></li>
+            <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
+            <li class="text-muted mb-1 fs-16">Ver registro</li>
+        </ul>
+    </div>
+</div>
+<!-- FIN CABECERA -->
+
+
+
+
+<!-- CONTENIDO -->
+
+<div class="row">
+	<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+
+			<div class="card-body">
+
+				<h4 class="mb-5 font-weight-semibold">Registro</h4>
+
+				<div class="row">
+					<div class="col-md-3">
+						<div class="form-group">
+							<label class="form-label">Nombre</label>
+							<div class="text-muted">{{$database->name}}</div>
+						</div>
+					</div>
+				<!--<div class="col-md-3">
+						<div class="form-group">
+							<label class="form-label">Localización</label>
+							<div class="text-muted">{{$database->location}}</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Numero de vistas</label>
+							<div class="text-muted">{{$database->views_counter}}</div>
+						</div>
+					</div>-->
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label class="form-label">Descripcion</label>
+							<div class="text-muted">{{$database->description}}</div>
+						</div>
+					</div>
+				</div>
+
+                <div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Costo total</label>
+							<div class="text-muted">{{$database->total_cost}}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- FIN CONTENIDO -->
+@endsection
