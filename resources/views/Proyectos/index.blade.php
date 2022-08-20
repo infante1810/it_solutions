5703dac2bc23d511df2c1f1de0063c705c4637df
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-20 16:22:12 -0500

    update proyects component

--- resources/views/Proyectos/index.blade.php
+++ resources/views/Proyectos/index.blade.php
@@ -5,14 +5,16 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Proyectos</h4>
+        <h4 class="page-title">Registros9</h4>
     </div>
     {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
     <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
             <div class="btn-list">
                 <a href="{{route('proyectos.create')}}" class="btn btn-primary mr-3">
-                    Agregar proyecto</a>
+                    <i class="fa-solid fa-plus"></i>
+                    Agregar Registro
+                </a>
             </div>
         </div>
     </div>
@@ -26,15 +28,18 @@
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
         <div class="card">
+            <div class="card-header  border-0">
+                <h4 class="card-title">Lista de productos/servicios</h4>
+            </div>
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table  table-vcenter text-wrap table-bordered border-bottom dt" id="project-list">
                         <thead>
                             <tr>
                                 <th class="border-bottom-0">ID</th>
-                                <th class="border-bottom-0">Proyecto</th>
+                                <th class="border-bottom-0">Servicio/Producto</th>
                                 <th class="border-bottom-0">Cliente</th>
-                                <th class="border-bottom-0">Fechas</th>
+                                <th class="border-bottom-0">Costos</th>
                                 <th class="border-bottom-0" Style="text-align: center;">Acciones</th>
                             </tr>
                         </thead>
@@ -43,24 +48,20 @@
                                 @foreach ($projects as $project)
                                 <tr>
                                     <td>{{$project->id}}</td>
-                                    <td><h6 class="mb-1 fs-14">{{$project->name}}</h6></td>
                                     <td>
                                         <div class="d-flex">
                                             <div class="mr-3 mt-0 mt-sm-1 d-block">
-                                                <h6 class="mb-1 fs-14">{{$project->customers->first_name}}</h6>
-                                                <p class="text-muted mb-0 fs-12">{{$project->customers->email}}</p>
+                                                <h6 class="mb-1 fs-14">{{$project->name}}</h6>
                                             </div>
                                         </div>
                                     </td>
                                     <td>
-                                        <div class="d-flex">
-                                            <div class="mr-3 mt-0 mt-sm-1 d-block">
-                                                <h6 class="mb-1 fs-14"><span class="text-muted mb-0 fs-14">Inicio: </span>{{$project->start_date}}</h6>
-                                                <h6 class="mb-1 fs-14"><span class="text-muted mb-0 fs-14">Fin: </span>{{$project->end_date}}</h6>
-                                            </div>
-                                        </div>
+                                        <span>{{$project->description}}</span>
                                     </td>
-                                    <td width='100px'>
+                                    <td>
+                                        <span>{{$project->total_cost}}</span>
+                                    </td>
+                                    <td>
                                         <div class="d-flex">
                                             <a href="{{route('proyectos.show', $project->id)}}" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Ver"><i class="fa-solid fa-eye text-primary"></i></a>
                                             <a href="{{route('proyectos.edit', $project->id)}}" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa-solid fa-pen text-success"></i></a>
@@ -83,4 +84,46 @@
     </div>
 </div>
 <!-- FIN CONTENIDO -->
+@endsection
+
+
+@section('extra-script')
+<script type="text/javascript">
+    function mensaje(){
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-outline-dark'
+          },
+          buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+          title: '¿Eliminar registro?',
+          text: "Si eliminas a este registro se eliminará permanentemente del sistema.",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Si, eliminar registro!.',
+          cancelButtonText: 'No, mantener registro!.',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              'El registro se ha eliminado correctamente.',
+              'success'
+            )
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire(
+              'Proceso cancelado.',
+              '',
+              'error'
+            )
+          }
+        })
+    }
+</script>
 @endsection
