4e3b93a27e72294e43984ef839b2a6b74637ba1d
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-20 15:41:00 -0500

    update vertical-menu

--- resources/views/components/menu-vertical.blade.php
+++ resources/views/components/menu-vertical.blade.php
@@ -12,7 +12,7 @@
 			<li class="slide">
 				<a class="side-menu__item {{ Request::is('/') ? 'active' : '' }}" data-toggle="slide" href="{{route('home')}}" id="/">
 					<i class="fa-solid fa-gauge mr-2"></i>
-					<span class="side-menu__label">Dashboard</span>
+					<span class="side-menu__label">Dashboard </span>
 				</a>
 			</li>
 
@@ -24,9 +24,15 @@
 			</li>
 
 			<li class="slide">
-				<a class="side-menu__item  {{ Request::is('bd') ? 'active' : '' }}" data-toggle="slide" href="{{route('bd.index')}}" id="bd">
+				<a class="side-menu__item {{ Request::is('registro2') ? 'active' : '' }}" data-toggle="slide" href="{{route('registro2.index')}}" id="modelos">
+					<i class="fa-solid fa-folder mr-2"></i>
+					<span class="side-menu__label">Area 2</span>
+				</a>
+			</li>
+			<li class="slide">
+				<a class="side-menu__item  {{ Request::is('registro3') ? 'active' : '' }}" data-toggle="slide" href="{{route('registro3.index')}}" id="bd">
 					<i class="fa-solid fa-database mr-2"></i>
-					<span class="side-menu__label">Bases de datos</span>
+					<span class="side-menu__label">Area 3</span>
 				</a>
 			</li>
 
@@ -34,70 +40,72 @@
             <li class="slide">
 				<a class="side-menu__item {{ Request::is('comentarios') ? 'active' : ''}}" data-toggle="slide" href="{{route('comentarios.index')}}" id="comentarios">
 					<i class="fa-solid fa-comment mr-2"></i>
-					<span class="side-menu__label">Comentarios</span>
+					<span class="side-menu__label">Area 4</span>
 				</a>
 			</li>
 
-            <li class="slide">
-				<a class="side-menu__item {{ Request::is('contactos') ? 'active' : ''}}" data-toggle="slide" href="{{route('contactos.index')}}" id="contactos">
-					<i class="fa-solid fa-address-book mr-2"></i>
-					<span class="side-menu__label">Contactos</span>
+			<li class="slide">
+				<a class="side-menu__item {{ Request::is('departamentos') ? 'active' : '' }}" data-toggle="slide" href="{{route('departamentos.index')}}" id="departamentos">
+					<i class="fa-solid fa-address-card mr-2"></i>
+					<span class="side-menu__label">Area 5</span>
 				</a>
 			</li>
+
+        <!--<li class="slide">
+				<a class="side-menu__item {{ Request::is('contactos') ? 'active' : ''}}" data-toggle="slide" href="{{route('contactos.index')}}" id="contactos">
+					<i class="fa-solid fa-address-book mr-2"></i>
+					<span class="side-menu__label">Area 5</span>
+				</a>
+			</li>-->
 
             <li class="slide">
 				<a class="side-menu__item {{ Request::is('contratos') ? 'active' : ''}}" data-toggle="slide" href="{{route('contratos.index')}}" id="contratos">
 					<i class="fa-solid fa-file-contract mr-2"></i>
-					<span class="side-menu__label">Contratos</span>
+					<span class="side-menu__label">Area 6</span>
 				</a>
 			</li>
 
-            <li class="slide">
+            <!-- <li class="slide">
 				<a class="side-menu__item {{ Request::is('departamentos') ? 'active' : '' }}" data-toggle="slide" href="{{route('departamentos.index')}}" id="departamentos">
 					<i class="fa-solid fa-address-card mr-2"></i>
-					<span class="side-menu__label">Departamentos</span>
+					<span class="side-menu__label">Area 6</span>
 				</a>
-			</li>
+			</li> -->
 
 			<li class="slide">
 				<a class="side-menu__item {{ Request::is('eventos') ? 'active' : '' }}" data-toggle="slide" href="{{route('eventos.index')}}" id="eventos">
 					<i class="fa-solid fa-calendar-day mr-2"></i>
-					<span class="side-menu__label">Eventos</span>
+					<span class="side-menu__label">Area 7</span>
 				</a>
 			</li>
 
-			<li class="slide">
+			<!-- <li class="slide">
 				<a class="side-menu__item {{ Request::is('empleados') ? 'active' : '' }}" data-toggle="slide" href="{{route('empleados.index')}}" id="empleados">
 					<i class="fa-solid fa-user mr-2"></i>
-					<span class="side-menu__label">Empleados</span>
+					<span class="side-menu__label">Area 8</span>
 				</a>
-			</li>
+			</li> -->
 
-			<li class="slide">
-				<a class="side-menu__item {{ Request::is('modelos') ? 'active' : '' }}" data-toggle="slide" href="{{route('modelos.index')}}" id="modelos">
-					<i class="fa-solid fa-folder mr-2"></i>
-					<span class="side-menu__label">Modelos</span>
-				</a>
-			</li>
+			
 
 			<li class="slide">
 				<a class="side-menu__item {{ Request::is('preguntas') ? 'active' : '' }}" data-toggle="slide" href="{{route('preguntas.index')}}" id="preguntas">
 					<i class="fa-solid fa-circle-question mr-2"></i>
-					<span class="side-menu__label">Preguntas</span>
+					<span class="side-menu__label">Area 8</span>
 				</a>
 			</li>
 
 			<li class="slide">
 				<a class="side-menu__item {{ Request::is('proyectos') ? 'active' : '' }}" data-toggle="slide" href="{{route('proyectos.index')}}" id="proyectos">
 					<i class="fa-solid fa-list-check mr-2"></i>
-					<span class="side-menu__label">Proyectos</span>
+					<span class="side-menu__label">Area 9</span>
 				</a>
 			</li>
 
 			<li class="slide">
-				<a class="side-menu__item {{ Request::is('riesgos') ? 'active' : '' }}" data-toggle="slide" href="{{route('riesgos.index')}}" id="riesgos">
+				<a class="side-menu__item {{ Request::is('registro10') ? 'active' : '' }}" data-toggle="slide" href="{{route('registro10.index')}}" id="riesgos">
 					<i class="fa-solid fa-triangle-exclamation mr-2"></i>
-					<span class="side-menu__label">Riesgos</span>
+					<span class="side-menu__label">Area 10</span>
 				</a>
 			</li>
 
