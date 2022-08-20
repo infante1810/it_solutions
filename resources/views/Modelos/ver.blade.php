957dc8ce89c070db558e1d8e34e692a005e8668c
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-20 15:19:09 -0500

    Update model view ver

--- resources/views/Modelos/ver.blade.php
+++ resources/views/Modelos/ver.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Ver modelo "N"</h4>
+    <h4 class="page-title">Ver  registro</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="#">Modelos</a></li>
+            <li class="mb-1 fs-16"><a href="{{ route('registro2.index') }}">Registro 2</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Ver modelos</li>
+            <li class="text-muted mb-1 fs-16">Ver registro</li>
         </ul>
     </div>
 </div>
@@ -18,40 +18,60 @@
 
 <!-- Row -->
 <div class="row">
-    <div class="col-8 col-md-12 col-lg-12">
-        <div class="card">
-               <div class="row">
-				<div class="col-5">
-						<div class="ml-sm-4 mt-5">
-							<div class="form-group">
-                                   <font style="font-weight:bold; vertical-align:inherit; padding-left:7px;">Modelo: </font>
-								<label style="padding-left:7px;" class="form-label">De negocio</label>
-							</div>
+	<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+
+			<div class="card-body">
+
+				<h4 class="mb-5 font-weight-semibold">Registro</h4>
+
+				<div class="row">
+					<div class="col-md-3">
+						<div class="form-group">
+							<label class="form-label">Nombre</label>
+							<div class="text-muted">{{$model->name}}</div>
+						</div>
+					</div>
+				<!--<div class="col-md-3">
+						<div class="form-group">
+							<label class="form-label">Localización</label>
+							<div class="text-muted">{{$model->location}}</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Numero de vistas</label>
+							<div class="text-muted">{{$model->views_counter}}</div>
+						</div>
+					</div>-->
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label class="form-label">Descripcion</label>
+							<div class="text-muted">{{$model->description}}</div>
 						</div>
 					</div>
 				</div>
-			<div class="card-body">
-                   <div class="row">
-                       <div class="col-md-4">
-                          <div class="form-group">
-                              <font style="vertical-align: inherit; font-weight:bold">Descripción: </font><br>
-                              <span class="mb-2 fs-14">Descripcion de modelo de negocio de la empresa Soluciones de TI</span>
-                          </div>
-                      </div>
-                      <div class="col-md-3">
-                      <div class="form-group">
-                          <font style="vertical-align: inherit; font-weight:bold">Fecha: </font><br>
-                          <span class="mb-2 fs-14">01-03-2022</span>
-                      </div>
-                   </div>
-                </div>
+
+                <div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Costo total</label>
+							<div class="text-muted">{{$model->total_cost}}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+			</div>
 		</div>
-
-                <div class="card-footer text-right">
-					<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
-					<i class="feather feather-corner-down-left sidemenu_icon"></i>Regresar</a>
-			    </div>
-	    </div>
 	</div>
-
+</div>
 @endsection
