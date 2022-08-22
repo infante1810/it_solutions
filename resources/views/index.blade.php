1bebb35cd971e4fe1dfff96e1a12dee6ac4404a6
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-21 20:52:27 -0500

    Update dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,11 +25,115 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
+        <div class="container">
+            <div class="row">
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">CLEMENTE GOMEZ </br> OSCAR ROBERTO </div>
+                                <div class="row text-muted">1930672</div>
+                                <div class="row">1930672@upv.edu.mx</div>
+                                <div class="row">Owner Area 8</div>                                    
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">DE LA FUENTE FLORES ESTEFANIA </div>
+                                <div class="row text-muted">1930019</div>
+                                <div class="row">1930019@upv.edu.mx</div>
+                                <div class="row">Owner Area 9</div>
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">GONZALEZ TOVAR </br> ANIBAL</div>
+                                <div class="row text-muted">1830612</div>
+                                <div class="row">1830612@upv.edu.mx</div>
+                                <div class="row">Owner Area 9</div> 
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">HERNANDEZ SANCHEZ </br>RAMON URIEL </div>
+                                <div class="row text-muted">1930138</div>
+                                <div class="row">1930138@upv.edu.mx</div>
+                                <div class="row">Owner Area 10</div>  
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">QUIÑONES MATA </br> JESUS EDUARDO</div>
+                                <div class="row text-muted">1930507</div>
+                                <div class="row">1930507@upv.edu.mx</div>
+                                <div class="row">Owner Area 7</div>  
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">TRISTAN SOTO </br> SANDRA YANETH</div>
+                                <div class="row text-muted">1930507</div>
+                                <div class="row">1930507@upv.edu.mx</div>
+                                <div class="row">Owner Area 7</div> 
+                            </div>
+                            
+                        </div>
+                </div>
+                <!--nuevo apartado de vista-->
+                <div class="col-5 mb-5 p-5 bg-white rounded shadow ml-7">
+                        <div class="d-flex">
+                            <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                <span class="avatar avatar-xl brround mr-3" style="background-image: url({{asset('images/infante.jpg')}}"></span>
+                            </div>
+                            <div>
+                                <div class="row fs-4 primary-font">INFANTE GONZALEZ </br> OSCAR ESTEBAN</div>
+                                <div class="row text-muted">2010008</div>
+                                <div class="row">2010008@upv.edu.mx</div>
+                                <div class="row">Owner Area 1</div>  
+                            </div>
+                            
+                        </div>
+                </div>
+            </div>
+                            
+        </div>
     </div>
 </div>
 <!-- FIN CONTENIDO -->
