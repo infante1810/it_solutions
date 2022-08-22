07711f2f984ea9c8f58b4d15d62b9ed6cf020698
Infante Gonzalez <60672197+infante1810@users.noreply.github.com>
2022-08-21 19:15:31 -0500

    update view ver comentarios

--- resources/views/Comentarios/ver.blade.php
+++ resources/views/Comentarios/ver.blade.php
@@ -5,7 +5,7 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">Ver  registro</h4>
+        <h4 class="page-title">Ver  registro #{{$comentario->id}}</h4>
         <ul class="breadcrumb">
             <li class="mb-1 fs-16"><a href="{{ route('registro4.index') }}">Registros 4</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
